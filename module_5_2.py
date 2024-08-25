36876e26620e57bea9be503b3e46311a487a4f65
Timofey1987 <son_decline@mail.ru>
2024-08-25 12:46:45 +0800

    Add files via upload

--- module_5_2.py
+++ module_5_2.py
@@ -1,13 +1,13 @@
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
-        self.numder_of_floors = number_of_floors
+        self.number_of_floors = number_of_floors
 
     def __len__(self):
-        return self.numder_of_floors
+        return self.number_of_floors
 
     def __str__(self):
-        return (f'Название: {self.name}, кол-во этажей: {self.numder_of_floors}')
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
 
 
